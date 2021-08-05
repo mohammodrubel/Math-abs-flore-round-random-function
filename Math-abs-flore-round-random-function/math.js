4422322a3d075ea9b8b2fae8bcfaf7cfc38919ef
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-05 23:09:19 +0600

    Add files via upload

---
+++ Math-abs-flore-round-random-function/math.js
@@ -0,0 +1,32 @@
+// abs function 
+// always return possitive value its call abs function 
+
+var myNumber = -3456.23
+
+var result = Math.abs(myNumber)
+
+console.log(result)
+
+// function returns the largest integer less than or equal to a given number.
+var myFlor = 346.345
+var myFlor2=90.87345
+var result1=Math.floor(myFlor)
+var result2 =Math.floor(myFlor2)
+console.log(result1)
+console.log(result2)
+
+// function returns the value of a number rounded to the nearest integer.
+var simple = 345.356
+var result = Math.round(simple)
+console.log(result)
+var simple2 = 23.80
+var result2=Math.round(simple2)
+console.log(result2)
+
+// Math.random give me a new random value , 
+var result= Math.random() * 10
+console.log(result)
+// and etc 
+// we are implement 2 function 1 Math.random and 2 Math.round 
+var mainResult = Math.round(result)
+console.log(mainResult)
